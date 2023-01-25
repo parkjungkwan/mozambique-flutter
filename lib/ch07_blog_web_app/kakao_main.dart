87894a763e0b544246d1d0ab2673e819573465c9
parkjungkwan <pakjkwan@gmail.com>
2023-01-25 17:28:57 +0900

    15

---
+++ lib/ch07_blog_web_app/kakao_main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:hello_flutter2/ch07_blog_web_app/screen/debug_page.dart';
+import 'package:hello_flutter2/ch07_blog_web_app/screen/main_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  KakaoSdk.init(
+    nativeAppKey: '030ba7c59137629e86e8721eb1a22fd6',
+    javaScriptAppKey: 'fa2d8e9f47b88445000592c9a293bbe2',
+    loggingEnabled: true,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (_) => MainPage(),
+        '/debug': (_) => DebugPage(),
+      },
+    );
+  }
+}
