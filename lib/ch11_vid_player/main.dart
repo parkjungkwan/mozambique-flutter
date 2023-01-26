45b60bae87e28563e91e50e856a4cb75115b713c
parkjungkwan <pakjkwan@gmail.com>
2023-01-26 14:48:55 +0900

    chapter 11 media player 완성

---
+++ lib/ch11_vid_player/main.dart
@@ -0,0 +1,11 @@
+import 'package:hello_flutter2/ch11_vid_player/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
