91931fcc1ae632c5dea6b07cd48e2351fae4a977
parkjungkwan <pakjkwan@gmail.com>
2023-02-01 16:09:03 +0900

    google map

---
+++ lib/ch13_chool_check/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:hello_flutter2/ch13_chool_check/screen/chool_check.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: ChoolCheck(),
+    ),
+  );
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import agora_rtc_engine
 import cloud_firestore
 import firebase_auth
 import firebase_core
+import geolocator_apple
 import iris_event
 import path_provider_foundation
 import shared_preferences_foundation
@@ -20,6 +21,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   IrisEventPlugin.register(with: registry.registrar(forPlugin: "IrisEventPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
